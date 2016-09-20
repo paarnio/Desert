a8dcf83f81227eef0bb544d1368ee4e44ffa13ee
paarnio <pekka.aarnio@aalto.fi>
2016-09-20 17:18:01 +0300

    VPA: Initial commit

---
+++ GitTestProject/src/siima/git/GitTest.java
@@ -0,0 +1,13 @@
+/*
+ * http://www.vogella.com/tutorials/EclipseGit/article.html#create-a-new-git-repository-via-eclipse
+ * 
+ * 
+ */
+
+package siima.git;
+
+public class GitTest {
+	public static void main(String[] args) {
+	    System.out.println("Git is fun");
+	  }
+}
