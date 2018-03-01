4eadaf40db2acf0115d6d7c28378c7d405171e42
paarnio <p.aarnio@gmail.com>
2018-03-01 13:34:08 +0200

    One: Added linkes to good youtube videos

--- GitTestProject/src/siima/git/GitTest.java
+++ GitTestProject/src/siima/git/GitTest.java
@@ -1,6 +1,12 @@
 /*
  * http://www.vogella.com/tutorials/EclipseGit/article.html#create-a-new-git-repository-via-eclipse
  * 
+ * 2018-03-01 Good videos:
+ * 1) simple:Merge, And Resolve Conflicts Using Git 
+ * https://www.youtube.com/watch?v=HZEb0rZk4Y8 
+ * 2)%% with branches: Remote Git Server and Merging:Git & Eclipse
+ * https://www.youtube.com/watch?v=KfeqnernMmE#t=808.04022
+ * 
  * Change A_one
  * Change X_oneY_two
  */
